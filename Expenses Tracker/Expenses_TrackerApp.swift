8723c8bd48e280f0a909b2ace4038e69304a68a8
ama <samaraweera97427@gmail.com>
2023-09-12 16:35:52 +0530

    Initial Commit

---
+++ Expenses Tracker/Expenses_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expenses_TrackerApp.swift
+//  Expenses Tracker
+//
+//  Created by Viraj Fernando on 2023-09-12.
+//
+
+import SwiftUI
+
+@main
+struct Expenses_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
